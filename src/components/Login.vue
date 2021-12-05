277da0dd0d3bc0ae08c9145a64ac7a96691861a9
Vitor Araujo <vitorgamer58@outlook.com>
2021-12-05 15:33:03 -0300

    Axios and Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="efetuarLogin">
+      <div>
+        <label for="email">Email</label>
+        <input type="email" class="form-control" v-model="usuario.email" />
+      </div>
+      <div>
+        <label for="password">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha" />
+      </div>
+      <button type="submit">Logar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {},
+      error: ''
+    }
+  },
+  name: "Login",
+  props: {},
+  methods: {
+    efetuarLogin() {
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/http/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = 'http://barrigarest.notasdovitor.top:3000'
+
+export default {
+    login (payload) {
+        const endpoint = `${BASE_URL}/auth/signin`
+
+        return axios.post(endpoint, payload)
+    },
+    cadastro (payload) {
+        const endpoint = `${BASE_URL}/auth/signup`
+
+        return axios.post(endpoint, payload)
+    }
+}
